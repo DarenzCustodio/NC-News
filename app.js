835a47e58ed972739952816a82d55fcba9d6f138
Darenz <darenz@MacBook-Air.local>
2024-01-15 18:43:31 +0000

    add feedback for task 2 GET/api/topics, add work along with tests for GET/api

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const app = express();
-app.use(express.json());
 
-const {getTopics} = require("./controllers/topics.controllers");
+const { getTopics, getEndpoints } = require("./controllers/topics.controllers");
 
 app.get("/api/topics", getTopics);
 
+app.get("/api", getEndpoints);
 
 app.use((err, req, res, next)=>{
     if(err.msg && err.status){
         res.status(err.status).send({msg: err.msg})
     }
 })
-
 
 module.exports = app;
