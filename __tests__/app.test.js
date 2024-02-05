6a5332f10a91a3fe53bde23ec2c632bc663f2e28
Darenz <darenz@MacBook-Air.local>
2024-02-05 17:17:29 +0000

    add task 12 along with test for /api/articles/:article_id (comment_count)

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -99,12 +99,12 @@ describe("GET /api/articles/article_id", () => {
         expect(article.title).toBe("Living in the shadow of a great man");
         expect(article.topic).toBe("mitch");
         expect(article.author).toBe("butter_bridge");
-        expect(article.body).toBe("I find this existence challenging");
         expect(article.created_at).toBe("2020-07-09T20:11:00.000Z");
         expect(article.votes).toBe(100);
         expect(article.article_img_url).toBe(
           "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
         );
+        expect(article.comment_count).toBe("11");
       });
   });
   test("returns status:400 with a error message when provided with non-existent article_id", () => {
